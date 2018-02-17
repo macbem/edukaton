cdaf08b0cba22b35f586ae98c9d306e259f46abb
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-02-17 16:21:07 +0100

    Added contact and nav-inside-menu contact is WIP, log in shows only when user is not logged in

---
+++ frontend/src/app/shared/nav/nav-inside-menu/nav-inside-menu.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../../auth/auth.service";
+
+@Component({
+  selector: 'app-nav-inside-menu',
+  templateUrl: './nav-inside-menu.component.html',
+  styleUrls: ['./nav-inside-menu.component.scss']
+})
+export class NavInsideMenuComponent implements OnInit {
+  isMenuActive = false;
+  isLoggedIn: boolean;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
+
+  toggleMenu(state) {
+    this.isMenuActive = state;
+  }
+
+}
