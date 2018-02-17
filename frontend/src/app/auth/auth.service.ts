ef5e2317d7d6a25c164c2d10fdc199d2d10eb7bc
macbem <maciej.bembenista@10clouds.com>
2018-02-17 13:01:18 +0100

    Implement basic auth service methods and TokenInterceptor for auth

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -1,8 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.prod';
+import { Observable } from 'rxjs/Observable';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getToken(): string {
+    return localStorage.getItem('authToken');
+  }
 
+  setToken(token?: string): void {
+    if (token) {
+      localStorage.setItem('authToken', token);
+    } else {
+      localStorage.removeItem('authToken');
+    }
+  }
+
+  signUp(email: string, password: string): Observable<any> {
+    const apiUrl = environment.apiUrl;
+
+    return this.http.post(`${apiUrl}/signup`, {
+      email,
+      password
+    });
+  }
+
+  logIn(email: string, password: string): Observable<any> {
+    const apiUrl = environment.apiUrl;
+
+    return this.http.post(`${apiUrl}/login`, {
+      email,
+      password
+    }).pipe(
+      tap(response => {
+        if (response.token) {
+          this.setToken(response.token);
+        }
+      }),
+    );
+  }
+
+  logOut() {
+    this.setToken();
+  }
 }
